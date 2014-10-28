e4693a32a767e0a30c6290e0420bba646d77c513
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2014-10-28 17:10:52 +0100

    Disable zooming in mobile version

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
     <link rel="shortcut icon" href="<?php echo base_url() ?>images/favicon.ico">
 	  <meta name="author" content="Ilias Trichopoulos">
